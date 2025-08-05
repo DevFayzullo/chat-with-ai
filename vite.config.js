d2c53c55f756c7a0a509c1c69273cada4f0f281c
DevFayzullo <fayzullo.coder@gmail.com>
2025-08-05 17:26:39 +0900

    deploy problem is solved from git hub pages nad netfily

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/chat-with-ai/",
+  base:
+    process.env.NODE_ENV === "production" &&
+    process.env.DEPLOY_TARGET === "gh-pages"
+      ? "/chat-with-ai/"
+      : "/",
 });
